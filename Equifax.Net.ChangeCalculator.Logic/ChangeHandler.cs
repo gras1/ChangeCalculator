7f631362d12dd3ebcc6814df8de10dffad108215
Graham Fearn <graham@fearnmarketingservices.com>
2022-01-06 21:04:42 +0000

    Got first BDD unit test passing

--- Equifax.Net.ChangeCalculator.Logic/ChangeHandler.cs
+++ Equifax.Net.ChangeCalculator.Logic/ChangeHandler.cs
@@ -4,6 +4,11 @@ public class ChangeHandler : IChangeHandler
 {
     public TransactionResponse CalculateChange(TransactionRequest request)
     {
+        var transactionResponse = new TransactionResponse(new Dictionary<Denomination, decimal>(), 0m);
+        if (request.AmountOfCash == request.Cost)
+        {
+            return transactionResponse;
+        }
         throw new NotImplementedException();
     }
 }
