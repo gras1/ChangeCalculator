1226a84be70fe2b75b92a25f2975b8f3258fc371
Graham Fearn <graham@fearnmarketingservices.com>
2022-01-07 20:11:35 +0000

    Started to add unit tests for ChangeCalculatorController

--- Equifax.Net.ChangeCalculator.Api.Tests/Controllers/ChangeCalculatorControllerTests.cs
+++ Equifax.Net.ChangeCalculator.Api.Tests/Controllers/ChangeCalculatorControllerTests.cs
@@ -2,9 +2,28 @@ namespace Equifax.Net.ChangeCalculator.Api.Tests.Controllers;
 
 public class ChangeCalculatorControllerTests
 {
-    [Fact]
-    public void Test1()
+    private readonly ILogger<ChangeCalculatorController> _logger;
+    private readonly IChangeHandler _changeHandler;
+    private readonly IChangeCalculationToTransactionResponseMapper _mapper;
+    private readonly IOptions<List<Denomination>> _options;
+    private readonly ChangeCalculatorController _controller;
+    
+    public ChangeCalculatorControllerTests()
     {
+        _logger = A.Fake<ILogger<ChangeCalculatorController>>();
+        _changeHandler = A.Fake<IChangeHandler>();
+        _mapper = A.Fake<IChangeCalculationToTransactionResponseMapper>();
+        _options = A.Fake<IOptions<List<Denomination>>>();
+        _controller = new ChangeCalculatorController(_logger, _changeHandler, _mapper, _options);
+    }
 
+    [Fact]
+    public void Post_WithNullRequest_ThrowsArgumentNullException()
+    {
+        //act
+        Action act = () => _controller.Post(default(TransactionRequest));
+
+        //assert
+        act.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'request')");
     }
 }
